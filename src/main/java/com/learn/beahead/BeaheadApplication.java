0f93c9fabde44e288f2c5025c65954eeae0cbde7
Sharma, Puneet [External] <Puneet.Sharma@externals.adidas-group.com>
2017-08-21 13:40:08 +0200

    inital commit

---
+++ src/main/java/com/learn/beahead/BeaheadApplication.java
@@ -0,0 +1,12 @@
+package com.learn.beahead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeaheadApplication.class, args);
+	}
+}
---
+++ src/test/java/com/learn/beahead/BeaheadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.beahead;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeaheadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
